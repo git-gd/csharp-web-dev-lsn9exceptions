dd2045847ca7748ee898651d206904f4ce56d332
Randy Parker <randallbparker@gmail.com>
2020-10-27 10:15:38 -0500

    working exceptions

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp_web_dev_lsn9exceptions
 {
@@ -8,17 +9,30 @@ namespace csharp_web_dev_lsn9exceptions
         static double Divide(double x, double y)
         {
             // Write your code here!
+            if (y == 0) throw new ArgumentOutOfRangeException("Cannot divide by 0.");
+
+            return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
-        }
+            if (String.IsNullOrEmpty(fileName)) throw new ArgumentNullException("fileName cannot be null or empty.");
 
+            return fileName.EndsWith(".cs") ? 1 : 0;
+        }
+        //If a student’s submitted file ends in .cs, they get 1 point. If a student’s submitted file doesn’t end in .cs, they get 0 points. If the file submitted is null or an empty string, an exception should be thrown. What kind of exception is up to you!
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Divide(1, 0);
+            } catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -26,8 +40,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+            students.Add("Charles", "ReadMe.txt");
 
-
+            foreach (string student in students.Keys)
+            {
+                try
+                {
+                    Console.WriteLine($"{student} submitted {students[student]} for a score of {CheckFileExtension(students[student])}.");
+                } catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(student + "'s assignment could not be graded: " + e.Message);
+                }
+            }
         }
     }
 }
